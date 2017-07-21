db989dce33a9f5a34d957b60858cb9032f51d66d
Ramon Sepulveda <gitjava@bmtask.com>
2017-07-21 11:59:18 -0500

    parcial, eliminar imagenes no utilizadas por meteor

--- src/iTunes.js
+++ src/iTunes.js
@@ -1,12 +1,51 @@
 import { execSync } from "child_process";
+import { join } from "path";
 import util from "./util";
 
+const settings = util.generateSettings(process.env);
+const outputDir = settings.METEOR_OUTPUT_ABSOLUTE;
+const xcode_name = settings.XCODE_SCHEME_NAME;
+
+const buildFolder = {
+  root: `${outputDir}/ios/project/${xcode_name}/Images.xcassets/AppIcon.appiconset`,
+};
+
+const unusedIcons = {
+  "icon.png"        : `${buildFolder.root}/icon.png`,
+  "icon@2x.png"     : `${buildFolder.root}/icon@2x.png`,
+  "icon-50.png"     : `${buildFolder.root}/icon-50.png`,
+  "icon-50@2x.png"  : `${buildFolder.root}/icon-50@2x.png`,
+  "icon-72.png"     : `${buildFolder.root}/icon-72.png`,
+  "icon-72@2x.png"  : `${buildFolder.root}/icon-72@2x.png`,
+}
+
+const cleanUnusedIcons = (env) => {
+  const contentsFile = join(buildFolder.root, "Contents.json");
+  let contentsVars = {};
+  try {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    contentsVars = require(contentsFile);
+    // eslint-disable-next-line no-empty
+    //console.log(contentsVars)
+  } catch (error) { return {}; }
+  for(let key in unusedIcons){
+    let value = unusedIcons[key];
+    
+    execSync(`rm -f ${value}`)
+  }
+  //console.log(contentsVars)
+}
+
 const uploadTestFlight = env => (
-  new Promise((resolve) => {
-    console.log(env)
+  new Promise((resolve,reject) => {
     if (!util.hasPlatform("ios",env)) {
       console.log("Skipping iOS upload to TestFlight...");
       return resolve("skipped");
+    }
+    try {
+      cleanUnusedIcons(env);
+    } catch (error) { 
+      return reject(error);
     }
     
     console.log("Uploading iOS to TestFlight...");
